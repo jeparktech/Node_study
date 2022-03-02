e3e5dbd850166eafb3a8028c799ad4d02c1725ce
Jeongwon Park <jepark044@gmail.com>
2022-03-02 17:48:16 -0500

    initial commit

---
+++ weather-app/app.js
@@ -0,0 +1,30 @@
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=07e120a909b24e61bf4bd2bd00f0bd2b&query=37.8267,-122.4233'
+
+// request({url: url, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         const current = response.body.current
+//         console.log(`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
+//     }
+    
+// })
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+const geoURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGp3MDQ0IiwiYSI6ImNsMGE0MWE2ZjA5MGszZHJ0bDlydms0dmEifQ.m1OVKbIlHji5DGSvHGNlWw&limit=1'
+
+request({url: geoURL, json: true}, (error, response) => {
+    if (error) {
+        console.log('error occured')
+    } else if ()
+    const feature = response.body.features[0]
+    const latitude = feature.center[1]
+    const longitude = feature.center[0]
+    console.log(`lat: ${latitude}, long: ${longitude}`)
+})
