c81a24f8ce6e3eb01af40235aed245fa35fafaf7
Jeongwon Park <jepark044@gmail.com>
2022-03-07 20:06:03 -0500

    .

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,35 @@
+// Object property shorthand
+
+const name = 'Jeongwon'
+const userAge = 23
+
+const user = {
+    name, //shorthand syntax
+    age: userAge,
+    location: 'Atlanta'
+}
+
+console.log(user)
+
+// Object destructuring
+
+const product = {
+    label: 'Red apple',
+    price: 3,
+    stock: 230,
+    salePrice: undefined
+}
+
+// const label = product.label
+// const stock = product.stock
+
+// const {label: productLabel, stock, rating = 5} = product
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+const transaction = (type, {label, stock}) => {
+    console.log(type, label, stock)
+}
+
+transaction('order', product)
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,33 +1,25 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url = 'http://api.weatherstack.com/current?access_key=07e120a909b24e61bf4bd2bd00f0bd2b&query=37.8267,-122.4233'
+const address = process.argv[2]
 
-// request({url: url, json: true}, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         const current = response.body.current
-//         console.log(`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
-//     }
-    
-// })
+if (!address) {
+    console.log('Please provide an address')
+    return
+}
 
-// Geocoding
-// Address -> Lat/Long -> Weather
-
-const geoURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGp3MDQ0IiwiYSI6ImNsMGE0MWE2ZjA5MGszZHJ0bDlydms0dmEifQ.m1OVKbIlHji5DGSvHGNlWw&limit=1'
-
-request({url: geoURL, json: true}, (error, response) => {
+// callback chaining
+geocode (address, (error, {latitude, longitude, location} = {}) => {
     if (error) {
-        console.log('error occured')
-    } else if (response.body.featues.length == 0) {
-        console.log('can not find the location')
-    } else {
-        const feature = response.body.features[0]
-        const latitude = feature.center[1]
-        const longitude = feature.center[0]
-        console.log(`lat: ${latitude}, long: ${longitude}`)
+        return console.log(error)
     }
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+        
+        console.log(location)
+        console.log(forecastData)
+      })
 })
